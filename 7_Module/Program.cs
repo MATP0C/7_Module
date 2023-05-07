c2ac41373980f5a3d26b193b8e068ead26828bf4
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-05-07 19:21:14 +0300

    7.5.9

--- 7_Module/Program.cs
+++ 7_Module/Program.cs
@@ -1,35 +1,36 @@
 ﻿using System;
 
-class A
+int num1 = -7;
+int num2 = 13;
+int num3 = -1;
+Console.WriteLine(num1.GetNegative());
+Console.WriteLine(num1.GetPositive());
+Console.WriteLine(num2.GetNegative());
+Console.WriteLine(num2.GetPositive());
+Console.WriteLine(num3.GetNegative());
+Console.WriteLine(num3.GetPositive());
+static class IntExtension
 {
-    public virtual void Display() 
+    public static int GetNegative(this int number)
     {
-        Console.WriteLine("A");
+        if(number > 0)
+        {
+            return - number;
+        }
+        else
+        {
+            return number;
+        }
     }
-}
-class B: A
-{
-     public new void Display()
+    public static int GetPositive(this int number)
     {
-        Console.WriteLine("B");
-    }
-}
-class C : A
-{
-    public override void Display()
-    {
-        Console.WriteLine("C");
-    }
-}
-class D: B
-{
-    public new void Display()
-    {
-    }
-}
-class E: C
-{
-    public new void Display()
-    {
+        if(number < 0)
+        {
+            return -number;
+        }
+        else
+        {
+            return number;
+        }
     }
 }
