982ad0550100e4527af9692c5d78d4a99b08df21
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-05-07 15:13:30 +0300

    Добавьте файлы проекта.

---
+++ 7_Module/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class A
+{
+    public virtual void Display() 
+    {
+        Console.WriteLine("A");
+    }
+}
+class B: A
+{
+     public new void Display()
+    {
+        Console.WriteLine("B");
+    }
+}
+class C : A
+{
+    public override void Display()
+    {
+        Console.WriteLine("C");
+    }
+}
+class D: B
+{
+    public new void Display()
+    {
+    }
+}
+class E: C
+{
+    public new void Display()
+    {
+    }
+}
