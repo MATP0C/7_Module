bff1604c548835593a64d38a0871fd49edacf9ea
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-05-09 15:39:10 +0300

    7.6.12

--- 7_Module/Program.cs
+++ 7_Module/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
-class Engine { }
-class CarPart { }
+abstract class Engine { }
+abstract class CarPart { }
 class ElectricEngine: Engine { }
 
 class GasEngine: Engine { }
@@ -12,11 +12,25 @@ class Differential: CarPart { }
 
 class Wheel: CarPart { }
 
-class Car <TEngine> where TEngine : Engine
+abstract class Car <TEngine> where TEngine : Engine
 {
     public TEngine Engine;
 
     public virtual void ChangePart<TPart>(TPart newPart) where TPart : CarPart
     {
+    }
+}
+class ElectricCar: Car<ElectricEngine>
+{
+    public override void ChangePart<TPart>(TPart newPart)
+    {
+
+    }
+}
+class GasCar: Car<GasEngine>
+{
+    public override void ChangePart<TPart>(TPart newPart)
+    {
+
     }
 }
